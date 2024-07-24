f7dbefec03fe261434bf27cb23716e363c88dc34
maria maria <decarvalholohana@gmail.com>
2024-07-23 21:17:16 -0400

    requisito #6 - link do id

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { listaDeCartas } from "./api/route";
 import Updating from "./Updating";
 import ErrorFetch from "./ErrorFetch";
--- src/app/card/page.js
+++ src/app/card/page.js
@@ -1,7 +1,53 @@
-import { useParams } from "next/navigation";
 
-export default function Card (){
-  return(
-    <p>id: {params.id} </p>
+import { useEffect, useState } from 'react';
+import Updating from '../Updating'; 
+import ErrorFetch from '../ErrorFetch'; 
+
+export default function CardDetail() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [card, setCard] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (id) {
+      const fetchCard = async () => {
+        try {
+          const response = await fetch(`/api/${id}`);
+          const data = await response.json();
+          setCard(data);
+          setIsLoading(false);
+        } catch (error) {
+          console.error("Erro ao buscar a carta:", error);
+          setIsError(true);
+          setIsLoading(false);
+        }
+      };
+      fetchCard();
+    }
+  }, [id]);
+
+  if (Updating) {
+    return <Updating />;
+  }
+
+  if (isError) {
+    return <ErrorFetch />;
+  }
+
+  if (!card) {
+    return <p>Carta não encontrada.</p>;
+  }
+
+  return (
+    <main>
+      <h1>{card.nome}</h1>
+      <p>Cor: {card.cor}</p>
+      <p>Descrição: {card.descricao}</p>
+      <p>Valor estimado: {card.valorEstimado}</p>
+      <p>Nível de raridade: {card.nivelRaridade}</p>
+      <p>Artista: {card.artista}</p>
+    </main>
   );
 }
